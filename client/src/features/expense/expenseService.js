32fbbd800594c03f07a6e33c90283b427d87c0c7
jatintomar04 <jatin.tomar.04@gmail.com>
2026-06-11 14:27:32 +0530

    socket

--- client/src/features/expense/expenseService.js
+++ client/src/features/expense/expenseService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/expense";
+const API_URL = "https://ai-splitter-0y72.onrender.com/api/expense";
 
 // GET ALL EXPENSES
 const getExpenses = async ( token) => {
