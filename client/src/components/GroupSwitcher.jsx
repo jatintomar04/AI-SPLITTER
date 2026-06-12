81370e9cb4014b7c4562e54a28a6f16f8debb99c
jatintomar04 <jatin.tomar.04@gmail.com>
2026-06-12 11:45:10 +0530

    group delete

--- client/src/components/GroupSwitcher.jsx
+++ client/src/components/GroupSwitcher.jsx
@@ -101,7 +101,7 @@ const GroupSwitcher = ({
                 </button>
 
                 {/* Leave Group */}
-                {user?._id !== group?.createdBy && (
+                {user?._id !== group?.createdBy?._id && (
                   <button
                     onClick={(e) => {
                       e.stopPropagation();
@@ -114,7 +114,7 @@ const GroupSwitcher = ({
                 )}
 
                 {/* Delete Group */}
-                {user?._id === group?.createdBy._id && (
+                {user?._id === group?.createdBy?._id && (
                   <button
                     onClick={(e) => {
                       e.stopPropagation();
